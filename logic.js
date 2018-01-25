1694afc153caef84cc42a9c3a5f7a589bc2cacb5
Kyle West <wes12009@byui.edu>
2018-01-24 20:04:11 -0700

    [SAVE STATE] I want to save my current progress before I branch off

--- logic.js
+++ logic.js
@@ -1,10 +1,32 @@
 function Logic (operator, values = []) {
-
+   this.operator = operator;
+   this.values = values;
 }
 
 Logic.prototype = {
    constructor: Logic,
    apply: function () {
-      console.log(`Applying "${operator}" on "${lhs}" and "${rhs}"`);
+      // console.log(`Applying "${operator}" on "${lhs}" and "${rhs}"`);
+   },
+
+   toString: function () {
+      return `{${this.operator}${(this.values[0]) ? ", " + this.values[0]: ""}${(this.values[1]) ? ", " + this.values[1]: ""}}`;
    }
+};
+
+var COMPOSIT_COUNTER = 0;
+
+function Value (symbol, isVariable = true) {
+   this.isVariable = isVariable;
+   this.isComposite = !this.isVariable;
+   if (this.isComposite) {
+      this.ref = COMPOSIT_COUNTER++;
+      this.symbol = symbol;
+   } else {
+      this.ref = symbol;
+   }
+}
+
+Value.prototype.toString = function () {
+   return this.ref
 };
--- truth.js
+++ truth.js
@@ -8,12 +8,18 @@ function createStack(text) {
 
    if (sub) {
       console.log(sub);
-      createStack(sub);
+      var sub_logic = createStack(sub);
+      var temp = text.replace(`(${sub})`, '{}');
+      var one = new Value(sub[sub.search(validVar)]);
+      var two = new Value()
+      console.log(`----> ${temp}, ${one}`);
+
    } else {
       lhs = text[0];
       rhs = text[text.length - 1];
       operator = text.slice(1, text.length - 1);
       console.log(`"${operator}" on "${lhs}" and "${rhs}"`);
+      return new Logic(operator, [new Value(lhs), new Value(rhs)]);
    }
 }
 
