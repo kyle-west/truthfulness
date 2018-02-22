1155653b806084fb46caaa345096e93c25d50773
Kyle West <wes12009@byui.edu>
2018-02-21 18:07:36 -0700

    [POLYFILLS] created an includeExtended function that checks objects

--- polyfills.js
+++ polyfills.js
@@ -1,3 +1,9 @@
+// determine the acutal type of an object
+function typeOf (obj) {
+  return Object.prototype.toString.call(obj).match(/\[object (.*)\]/)[1];
+}
+
+
 // Repeat a string a certain amount of times.
 // Example: "A".repeat(3) ---> returns "AAA"
 String.prototype.repeat = function (times) {
@@ -8,13 +14,41 @@ String.prototype.repeat = function (times) {
    return str;
 }
 
+
+// return an ordered set of the values in the array.
+Array.prototype.includesExtended = function (item) {
+   var isObj = (typeOf(item) === "Object");
+   for (var i = 0; i < this.length; i++) {
+      if (isObj && typeOf(this[i]) === "Object") {
+         if (this[i].equals(item)) return true;
+      } else {
+         if (this[i] === item) return true;
+      }
+   }
+   return false;
+}
+
+
 // return an ordered set of the values in the array.
 Array.prototype.getUnique = function () {
    var unique = [];
    this.forEach(elem => {
-      if (!unique.includes(elem)) {
+      if (!unique.includesExtended(elem)) {
          unique.push(elem);
       }
    });
    return unique;
+}
+
+
+// find if two objects are equal
+Object.prototype.equals = function (elem) {
+   var lhs = Object.getOwnPropertyNames(this),
+       rhs = Object.getOwnPropertyNames(elem),
+       i = 0;
+   if (lhs.length !== rhs.length) return false;
+   for (i; i < rhs.length; i++) {
+      if (this[rhs[i]] !== elem[rhs[i]]) return false;
+   }
+   return true;
 }
