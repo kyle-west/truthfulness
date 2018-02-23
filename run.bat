3b37e82a2e67018a5411b243b98f5f5a68892be2
Kyle West <wes12009@byui.edu>
2018-02-23 07:27:18 -0700

    [SERVER] updated run.bat and created run.sh

--- run.bat
+++ run.bat
@@ -1,3 +1,3 @@
-REM this and the 'server.py' script are only around so that I can use my typical
-REM web development workflow. They do not add any other functionality.
+:: this and the 'server.py' script are only around so that I can use my typical
+:: web development workflow. They do not add any other functionality.
 python server.py
---
+++ run.sh
@@ -0,0 +1,3 @@
+#  this and the 'server.py' script are only around so that I can use my typical
+#  web development workflow. They do not add any other functionality.
+python server.py
